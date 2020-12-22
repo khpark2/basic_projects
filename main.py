682d6cac06318391ae8e8ce683b4e6ec7a29d1cd
Keon Ho Ho <keonho.roks@gmail.com>
2020-12-22 13:54:57 +0000

    added render function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import tictactoe
 
-tictactoe.new_board()
+board = tictactoe.new_board()
+board[0][1] = 'X'
+board[1][1] = 'O'
+tictactoe.render(board)
--- tictactoe.py
+++ tictactoe.py
@@ -4,10 +4,25 @@ def new_board():
     i = 0
     j = 0
     while i < 3:
+        row_board = []
+        j = 0
         while j < 3:
             row_board.append(None)
             j += 1
         board.append(row_board)
         i += 1
-    print(board)
+    return(board)
 
+def render(board):
+    print('  1 2 3')
+    print('  ' + ('-' * 6))
+    rendered_row = []
+    for y in range(3): # iterate for each row
+        for x in range(3): # iterate for each element in each row
+            if board[x][y] == None:
+                rendered_row.append(' ')
+            else:
+                rendered_row.append(board[x][y])
+        print(str(y+1) + '|' + ' '.join(rendered_row) + '|')
+        rendered_row = []
+    print('  ' + ('-' * 6))
