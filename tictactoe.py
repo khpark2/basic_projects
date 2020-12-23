c4462b4f021c95b382741ece2f82d68cd6d5568b
Keon Ho Ho <keonho.roks@gmail.com>
2020-12-23 10:23:28 +0000

    Added check_winner and updated play()

--- tictactoe.py
+++ tictactoe.py
@@ -11,37 +11,41 @@ def new_board():
             j += 1
         board.append(row_board)
         i += 1
-    return(board)
+    return (board)
+
 
 def render(board):
-    print('  1 2 3')
+    print('  0 1 2')
     print('  ' + ('-' * 6))
     rendered_row = []
-    for y in range(3): # iterate for each row
-        for x in range(3): # iterate for each element in each row
+    for y in range(3):  # iterate for each row
+        for x in range(3):  # iterate for each element in each row
             if board[x][y] == None:
                 rendered_row.append(' ')
             else:
                 rendered_row.append(board[x][y])
-        print(str(y+1) + '|' + ' '.join(rendered_row) + '|')
+        print(str(y) + '|' + ' '.join(rendered_row) + '|')
         rendered_row = []
     print('  ' + ('-' * 6))
+
 
 def get_move():
     user_move = input("Type in your move in the format x, y and press ENTER: ")
     formatted_move = eval(user_move)
     return formatted_move
 
+
 def make_move(board, player):
 
     while True:
         x, y = get_move()
-        if board[x][y] != None:
+        if board[x][y] == None:
             board[x][y] = player
             break
         else:
             print("That is an invalid move!")
-    return(board)
+    return (board)
+
 
 def play():
     board = new_board()
@@ -57,3 +61,30 @@ def play():
         elif player_turn == Player2:
             player_turn = Player1
         turn_counter += 1
+        if get_winner(board) == None:
+            pass
+        elif get_winner(board) == 'X' or get_winner(board) == 'O':
+            print(get_winner(board))
+            break
+    if get_winner(board) == None:
+        print(get_winner(board))
+
+
+def get_winner(board):
+    for row in board:
+        if row[0] == row[1] and row[0] == row[2]:
+            return str(row[0])
+            break
+        else:
+            pass
+    for column in range(3):
+        if board[0][column] == board[1][column] and board[0][column] == board[2][column]:
+            return str(board[0][column])
+            break
+        else:
+            pass
+    if board[0][0] == board[1][1] and board[0][0] == board[2][2]:
+        return str(board[0][0])
+    elif board[0][2] == board[1][1] and board[0][2] == board[2][0]:
+        return str(board[1][1])
+
